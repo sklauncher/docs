1cc5be8086928c96ad9430651a63a1bb7479cfe4
skmedix <skmedixs@gmail.com>
2022-07-30 22:41:46 +0200

    :wrench: Changed social links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,16 +96,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://discord.gg/BdCcpDZ',
               },
             ],
           },
