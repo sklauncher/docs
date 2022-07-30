fb726b545ad7cd42e4974450102ed5a9d3dfd6c2
skmedix <skmedixs@gmail.com>
2022-07-30 22:41:35 +0200

    :wrench: Fixed edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sklauncher/skl-wiki/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sklauncher/skl-wiki/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
